0a88e882fd0311663a61f911e0a18380744ba581
sujandthakare <sujanthakare92@gmail.com>
2016-10-05 17:46:56 +0530

    fixed for method issue and confirmation email issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :layout_by_resource
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
 
   protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
   def layout_by_resource
     if devise_controller?
       "session"
