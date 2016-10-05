f6c64c033ec7fb7ec6658c0edffa1d5662d161a4
sujandthakare <sujanthakare92@gmail.com>
2016-10-05 17:35:33 +0530

    Created layouts added toastr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,23 @@
 //= require jquery
 //= require jquery_ujs
 //= require semantic-ui
+//= require toastr.min
 //= require_tree .
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "4000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  protected
+  def layout_by_resource
+    if devise_controller?
+      "session"
+    else
+      "application"
+    end
+  end
 end
