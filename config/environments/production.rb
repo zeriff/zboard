b276d0fae83c67215d177a54c3c8b55da10d619a
sujandthakare <sujanthakare92@gmail.com>
2016-10-24 22:41:44 +0530

    send grid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: 'calm-stream-27400.herokuapp.com'}
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+
+  # config.action_mailer.delivery_method = :letter_opener
 end
