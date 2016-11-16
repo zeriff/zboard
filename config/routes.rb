0dcc7b795dcccda27a26cff7b0810147d1e734b0
sujandthakare <sujanthakare92@gmail.com>
2016-11-16 09:11:23 +0530

    changed youtube api

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :pins
 
+  # get 'api/pins', to: "pins#pins"
   devise_for :users, controllers: {registrations: 'registrations'}
 
   root to: 'home#index'
