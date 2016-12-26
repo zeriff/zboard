e1616177f7c298d17cd15cb6848a34e08ad8d2bd
sujandthakare <sujanthakare92@gmail.com>
2016-12-26 17:40:43 +0530

    routes for dotted

--- config/routes.rb
+++ config/routes.rb
@@ -1,71 +1,68 @@
 Rails.application.routes.draw do
+    root to: 'home#index'
 
-  root to: 'home#index'
+    # **************Devise***********************
+    # ********************************************
+    devise_for :users, controllers: { registrations: 'registrations' }
 
-  # **************Devise***********************
-  # ********************************************
-  devise_for :users, controllers: {registrations: 'registrations'}
+    resources :professions
+    resources :boards
+    resources :categories
 
-  resources :professions
-  resources :boards
-  resources :categories
+    # **************Home_controller************
+    # ******************************************
+    get '/:username/follows', to: 'home#follows', as: :user_follows
+    get '/:username/followers', to: 'home#followers', as: :user_followers
+    get '/:username/savedpins', to: 'home#savedpins', as: :saved_pins
+    get '/findfriend', to: 'home#findfriend', as: :findfriend
+    get '/users/:username', to: 'home#profile', as: :profile, constraints: { username: /[^\/]+/ }
+    get '/:username/board/:board_id', to: 'home#board', as: :user_board, constraints: { username: /[^\/]+/, board_id: /[^\/]+/ }
+    get '/:username/pins', to: 'home#pins', as: :user_pins, constraints: { username: /[^\/]+/ }
+    get '/:username/followers', to: 'home#followers', as: :follower_list, constraints: { username: /[^\/]+/ }
+    get '/:username/follows', to: 'home#follows', as: :follow_list, constraints: { username: /[^\/]+/ }
 
-  #**************Home_controller************
-  #******************************************
-  get "/:username/follows", to: 'home#follows', as: :user_follows
-  get "/:username/followers", to: 'home#followers', as: :user_followers
-  get "/:username/savedpins", to: 'home#savedpins', as: :saved_pins
-  get '/findfriend', to: "home#findfriend", as: :findfriend
-  get '/users/:username', to: "home#profile", as: :profile
-  get '/:username/board/:board_id', to: "home#board", as: :user_board
-  get '/:username/pins', to: "home#pins", as: :user_pins
-  get '/:username/followers', to: "home#followers", as: :follower_list
-  get '/:username/follows', to: "home#follows", as: :follow_list
+    # *************Pins_controller************
+    # ******************************************
+    resources :pins, except: [:index]
+    post 'pin/getinfo', to: 'pins#getinfo', as: 'pininfo'
+    get 'pins/category/:category_id', to: 'pins#getbycategory', as: 'categorize'
 
-  # *************Pins_controller************
-  #******************************************
-  resources :pins, except: [:index]
-  post 'pin/getinfo', to: 'pins#getinfo', as: "pininfo"
-  get 'pins/category/:category_id', to: 'pins#getbycategory', as: "categorize"
+    # *************Likes_controller************
+    # ******************************************
+    get 'like/status/:id', to: 'likes#get_like_status'
+    get 'like/:id', to: 'likes#create', as: :likes
+    delete 'like/:id', to: 'likes#destroy', as: :like
 
-  # *************Likes_controller************
-  #******************************************
-  get 'like/status/:id', to: 'likes#get_like_status'
-  get 'like/:id', to: "likes#create", as: :likes
-  delete 'like/:id', to: "likes#destroy", as: :like
+    # *************Save_controller************
+    # ******************************************
+    get 'save/status/:id', to: 'save#get_save_status'
+    get 'save/:id', to: 'save#create', as: :saves
+    delete 'save/:id', to: 'save#destroy', as: :save
 
-  # *************Save_controller************
-  #******************************************
-  get 'save/status/:id', to: 'save#get_save_status'
-  get 'save/:id', to: "save#create", as: :saves
-  delete 'save/:id', to: "save#destroy", as: :save
+    # *************Follows_controller************
+    # ******************************************
+    get 'follow/status/:id', to: 'follows#get_follow_status'
+    get 'follow/:id', to: 'follows#create', as: :follows
+    delete 'follow/:id', to: 'follows#destroy', as: :follow
+    delete 'follow/delete/:id', to: 'follows#delete', as: :del_currupt_follow
 
-  # *************Follows_controller************
-  #******************************************
-  get "follow/status/:id", to: "follows#get_follow_status"
-  get 'follow/:id', to: "follows#create", as: :follows
-  delete 'follow/:id', to: "follows#destroy", as: :follow
-  delete 'follow/delete/:id', to: "follows#delete", as: :del_currupt_follow
+    # *************Search_controller************
+    # ******************************************
+    get '/searchlocations/:query', to: 'search#locationSearch', as: :locationsearch
+    get '/search', to: 'search#search', as: :search
+    get '/find/:query', to: 'search#find'
 
-  # *************Search_controller************
-  #******************************************
-  get "/searchlocations/:query", to: "search#locationSearch", as: :locationsearch
-  get "/search", to: "search#search", as: :search
-  get "/find/:query", to: "search#find"
+    # *************Superuser_controller************
+    # ******************************************
+    get 'superuser/dashboard'
+    delete 'superuser/delete/user/:id', to: 'superuser#delete_user', as: :destroy_user
+    get 'superuser/createadmin/:id', to: 'superuser#create_admin', as: :create_admin
+    get 'superuser/manage_users', to: 'superuser#manage_users', as: :manage_users
+    get 'superuser/manage_pins', to: 'superuser#manage_pins', as: :manage_pins
+    get 'superuser/manage_categories', to: 'superuser#manage_categories', as: :manage_categories
+    get 'superuser/manage_currupt', to: 'superuser#manage_currupt', as: :manage_currupt
 
-  # *************Superuser_controller************
-  #******************************************
-  get 'superuser/dashboard'
-  delete 'superuser/delete/user/:id', to: "superuser#delete_user", as: :destroy_user
-  get 'superuser/createadmin/:id', to: "superuser#create_admin", as: :create_admin
-  get 'superuser/manage_users', to: 'superuser#manage_users', as: :manage_users
-  get 'superuser/manage_pins', to: 'superuser#manage_pins', as: :manage_pins
-  get 'superuser/manage_categories', to: 'superuser#manage_categories', as: :manage_categories
-  get 'superuser/manage_currupt', to: 'superuser#manage_currupt', as: :manage_currupt
-
-
-  #*****************API ROUTES*******************
-  #**********************************************
-  get 'api/get_saved', to: 'home#get_saved'
-
+    # *****************API ROUTES*******************
+    # **********************************************
+    get 'api/get_saved', to: 'home#get_saved'
 end
